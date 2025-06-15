7fffbee024744a5a48840941919c3583b859f633
its-tusharr <tusharverma2420@gmail.com>
2025-06-15 11:54:35 +0000

    save

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+// dataTypes
+
+/*premitive*/
+// nubmber
+let age = 20
+// string
+let name = "tushar"
+// boolean
+let loginfo = true/false
+// null 
+// (it means no value)
+// undefine
+let state;
+// symbol
+
+/*non-premitive*/
+// object
