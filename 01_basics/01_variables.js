65348f50fe31ce40b908a6298f431fd04a7a2f5c
its-tusharr <tusharverma2420@gmail.com>
2025-06-15 11:04:15 +0000

    save

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,2 +1,8 @@
-const accountId = 14545
-let accountEmail = ""
+const accountId = 1454
+let accountEmail = "myg@gmail.com"
+let accountCity = "jaipur"
+
+// accountEmail = "your@gmail.com"
+// accountCity = "bangaluru"
+
+console.table([accountId, accountEmail, accountCity])
